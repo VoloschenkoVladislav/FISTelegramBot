3bc7c5bac719f0f35297c318939cb2528dd4a011
Владислав <voloshenko_vladislav@mail.ru>
2024-12-25 19:12:16 +0700

    move setting to app_settings conf file

---
+++ src/bot.py
@@ -0,0 +1,39 @@
+import telebot
+from telebot.types import ReactionTypeEmoji
+import os
+import json
+
+
+with open(os.path.dirname(os.path.abspath(__file__)) + '/../conf/app_settings.json', encoding='utf-8') as settings_file:
+  settings = json.load(settings_file)
+
+print(settings['outer_chat_id'])
+
+bot = telebot.TeleBot(settings['bot_id'])
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+  bot.send_message(message.chat.id, settings['response_messages']['start_message'])
+
+@bot.message_handler(content_types=['text'])
+def forward_message(message):
+  if message.text.startswith(settings['trigger_tag']) and message.chat.id != settings['outer_chat_id']:
+    try:
+      bot.forward_message(settings['outer_chat_id'], message.chat.id, message.message_id)
+    except:
+      bot.send_message(message.chat.id, settings['response_messages']['send_error'])
+      return
+    if settings['response_types']['emoji']:
+      try:
+        bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(settings['response_messages']['response_emoji'])], is_big=False)
+      except:
+        bot.send_message(message.chat.id, settings['response_messages']['emoji_response_error'])
+        return
+    if settings['response_types']['text']:
+      try:
+        bot.send_message(message.chat.id, settings['response_messages']['send_success'])
+      except:
+        bot.send_message(message.chat.id, settings['response_messages']['text_response_error'])
+        return
+
+bot.infinity_polling()
