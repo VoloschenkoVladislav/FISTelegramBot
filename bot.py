1122f5f62af5f8bcf8bac7e68a063039f4c6c263
Владислав <voloshenko_vladislav@mail.ru>
2024-12-25 17:19:56 +0700

    add trigger tag to env variables

--- bot.py
+++ bot.py
@@ -7,15 +7,20 @@ import os
 load_dotenv()
 BOTID = os.getenv('BOTID')
 CHATID = os.getenv('CHATID')
+TRIGGER_TAG = os.getnev('TRIGGER_TAG')
 EMOJI_REACTION = os.getenv('EMOJI_REACTION').lower() in ('true', '1', 't')
 TEXT_REACTION = os.getenv('TEXT_REACTION').lower() in ('true', '1', 't')
 TEXT_REACTION_VALUE = os.getenv('TEXT_REACTION_VALUE')
 
 bot = telebot.TeleBot(BOTID)
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+  bot.send_message(message.chat.id, 'FISModelBot активен. Версия 1.0.0.')
+
 @bot.message_handler(content_types=['text'])
-def show_message(message):
-  if message.text.startswith('#изменение_модели\n') and message.chat.id != CHATID:
+def forward_message(message):
+  if message.text.startswith(TRIGGER_TAG) and message.chat.id != CHATID:
     try:
       bot.forward_message(CHATID, message.chat.id, message.message_id)
     except:
