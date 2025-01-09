51db4cacfd249396da092195a700ae45b4347b9d
Владислав <voloshenko_vladislav@mail.ru>
2025-01-09 15:42:16 +0700

    add forum chat support

--- src/bot.py
+++ src/bot.py
@@ -2,37 +2,191 @@ import telebot
 from telebot.types import ReactionTypeEmoji
 import os
 import json
+from utils import get_thread_id, is_reciever, is_forwarder
 
+
+if not os.path.exists(os.path.dirname(os.path.abspath(__file__)) + '/../conf/chat_settings.json'):
+  with open(os.path.dirname(os.path.abspath(__file__)) + '/../conf/chat_settings.json', 'w'): pass
 
 with open(os.path.dirname(os.path.abspath(__file__)) + '/../conf/app_settings.json', encoding = 'utf-8') as settings_file:
   settings = json.load(settings_file)
 
+with open(os.path.dirname(os.path.abspath(__file__)) + '/../conf/chat_settings.json', 'r+', encoding = 'utf-8') as chat_settings_file:
+  try:
+    chat_settings = json.load(chat_settings_file)
+  except json.decoder.JSONDecodeError:
+    json.dump({'recievers': [], 'forwarders': []}, chat_settings_file)
+    chat_settings = json.loads('{"recievers": [], "forwarders": []}')
+
+
 bot = telebot.TeleBot(settings['bot_id'])
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-  bot.send_message(message.chat.id, settings['response_messages']['start_message'])
+  bot.send_message(message.chat.id, settings['response_messages']['start_message'], message_thread_id = get_thread_id(message))
+
+
+@bot.message_handler(commands=['status'])
+def status(message):
+  reciever_status = 'вкл. ⬇️' if is_reciever(message, chat_settings) else 'выкл. ⏸'
+  forwarder_status = 'вкл. ⬆️' if is_forwarder(message, chat_settings) else 'выкл. ⏸'
+  bot.send_message(message.chat.id, 'Статус чата:\nОтправка сообщений ' + forwarder_status + '\nПриём сообщений ' + reciever_status, message_thread_id = get_thread_id(message))
+
+
+@bot.message_handler(commands=['enable_recieve'])
+def enable_recieve(message):
+  thread_id = get_thread_id(message)
+
+  if is_reciever(message, chat_settings):
+    bot.send_message(
+      message.chat.id,
+      settings['response_messages']['already_in_recievers'],
+      message_thread_id = thread_id
+    )
+  else:
+    chat_settings['recievers'].append({
+      'chat_id': message.chat.id,
+      'thread_id': thread_id
+    })
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/../conf/chat_settings.json', 'w', encoding = 'utf-8') as chat_settings_file:
+      json.dump(chat_settings, chat_settings_file)
+    bot.send_message(
+      message.chat.id,
+      settings['response_messages']['added_to_recievers'],
+      message_thread_id = thread_id
+    )
+
+
+@bot.message_handler(commands=['disable_recieve'])
+def disable_recieve(message):
+  thread_id = get_thread_id(message)
+
+  if not is_reciever(message, chat_settings):
+    bot.send_message(
+      message.chat.id,
+      settings['response_messages']['already_excluded_from_recievers'],
+      message_thread_id = thread_id
+    )
+  else:
+    chat_settings['recievers'].remove({
+      'chat_id': message.chat.id,
+      'thread_id': thread_id
+    })
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/../conf/chat_settings.json', 'w', encoding = 'utf-8') as chat_settings_file:
+      json.dump(chat_settings, chat_settings_file)
+    bot.send_message(
+      message.chat.id,
+      settings['response_messages']['excluded_from_recievers'],
+      message_thread_id = thread_id
+    )
+
+
+@bot.message_handler(commands=['enable_forward'])
+def enable_forward(message):
+  thread_id = get_thread_id(message)
+
+  if is_forwarder(message, chat_settings):
+    bot.send_message(message.chat.id, settings['response_messages']['already_in_forwarders'], message_thread_id = thread_id)
+  else:
+    chat_settings['forwarders'].append({
+      'chat_id': message.chat.id,
+      'thread_id': thread_id
+    })
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/../conf/chat_settings.json', 'w', encoding = 'utf-8') as chat_settings_file:
+      json.dump(chat_settings, chat_settings_file)
+    bot.send_message(
+      message.chat.id,
+      settings['response_messages']['added_to_forwarders'],
+      message_thread_id = thread_id
+    )
+
+
+@bot.message_handler(commands=['disable_forward'])
+def disable_forward(message):
+  thread_id = get_thread_id(message)
+
+  if not is_forwarder(message, chat_settings):
+    bot.send_message(
+      message.chat.id,
+      settings['response_messages']['already_excluded_from_forwarders'],
+      message_thread_id = thread_id
+    )
+  else:
+    chat_settings['forwarders'].remove({
+      'chat_id': message.chat.id,
+      'thread_id': thread_id
+    })
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/../conf/chat_settings.json', 'w', encoding = 'utf-8') as chat_settings_file:
+      json.dump(chat_settings, chat_settings_file)
+    bot.send_message(
+      message.chat.id,
+      settings['response_messages']['excluded_from_forwarders'],
+      message_thread_id = thread_id
+    )
+
 
 @bot.message_handler(content_types=['text'])
 def forward_message(message):
-  for chat_id in settings['chats_ids']:
-    if message.text.startswith(settings['trigger_tag']) and message.chat.id != chat_id:
-      try:
-        bot.forward_message(chat_id, message.chat.id, message.message_id)
-      except:
-        bot.send_message(message.chat.id, settings['response_messages']['send_error'])
-        return
-  if settings['response_types']['emoji']:
-    try:
-      bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(settings['response_messages']['response_emoji'])], is_big = False)
-    except:
-      bot.send_message(message.chat.id, settings['response_messages']['emoji_response_error'])
-      return
-  if settings['response_types']['text']:
-    try:
-      bot.send_message(message.chat.id, settings['response_messages']['send_success'])
-    except:
-      bot.send_message(message.chat.id, settings['response_messages']['text_response_error'])
-      return
+  thread_id = get_thread_id(message)
+  
+  if is_forwarder(message, chat_settings):
+    for chat in chat_settings['recievers']:
+      if (
+        message.text.startswith(settings['trigger_tag'])
+        and (
+          message.chat.id == chat['chat_id']
+          and thread_id != chat['thread_id']
+          and settings['send_to_same_chat']
+          or
+          message.chat.id != chat['chat_id']
+        ) 
+      ):
+        try:
+          bot.forward_message(
+            chat['chat_id'],
+            message.chat.id,
+            message.message_id,
+            message_thread_id = chat['thread_id']
+          )
+          if settings['response_types']['emoji']:
+            try:
+              bot.set_message_reaction(
+                message.chat.id,
+                message.id,
+                [ReactionTypeEmoji(settings['response_messages']['response_emoji'])],
+                is_big = False
+              )
+            except:
+              bot.send_message(message.chat.id, settings['response_messages']['emoji_response_error'], message_thread_id = thread_id)
+              break
+          if settings['response_types']['text']:
+            try:
+              bot.send_message(
+                message.chat.id,
+                settings['response_messages']['send_success'],
+                message_thread_id = thread_id
+              )
+            except:
+              bot.send_message(
+                message.chat.id,
+                settings['response_messages']['text_response_error'],
+                message_thread_id = thread_id
+              )
+              break
+        except Exception as error:
+          if settings['show_error_details']:
+            bot.send_message(
+              message.chat.id,
+              settings['response_messages']['send_error'] + '\n\n' + f'Unexpected {error=}, {type(error)=}',
+              message_thread_id = thread_id
+            )
+          else:
+            bot.send_message(
+              message.chat.id,
+              settings['response_messages']['send_error'],
+              message_thread_id = thread_id
+            )
+          break
 
 bot.infinity_polling()
---
+++ src/utils.py
@@ -0,0 +1,26 @@
+def get_thread_id(message):
+  try:
+    thread_id = message.reply_to_message.message_thread_id
+  except AttributeError:
+    thread_id = None
+  return thread_id
+
+def is_reciever(message, chat_settings):
+  thread_id = get_thread_id(message)
+  for reciever in chat_settings['recievers']:
+    if (
+      reciever['chat_id'] == message.chat.id
+      and reciever['thread_id'] == thread_id
+    ):
+      return True
+  return False
+
+def is_forwarder(message, chat_settings):
+  thread_id = get_thread_id(message)
+  for forwarder in chat_settings['forwarders']:
+    if (
+      forwarder['chat_id'] == message.chat.id
+      and forwarder['thread_id'] == thread_id
+    ):
+      return True
+  return False
